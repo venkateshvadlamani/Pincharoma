07a84abb594c171d5d99d493c8f7428e88cac882
venkatesh <venkatesh.caretiger@gmail.com>
2018-06-01 06:05:51 -0500

    added some more styling and fixed some of the navbar syling for mobile

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,9 +2,18 @@
   <div class="test">
     <h1 class="title">Different Experiments from Jen Simmons lab</h1>
 
-    <section class="">
-        
+    <section class="media-query-test">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
     </section>
+
+    <section class="rafaelaferro">
+
+    </section>
+
 
   </div>
 </template>

b28a57de2a4fefb77c0716a4e7ae75e0cd4ffb4b
venkatesh <venkatesh.caretiger@gmail.com>
2018-06-02 07:44:42 -0500

    added the panels for the 4 features

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,42 @@
 <template>
   <div class="home">
 
-    <div class="spices"><h1>SPICES</h1></div>
+    <section class="main">
+      <div class="spices"><h1>SPICES</h1></div>      
+      <div class="limited-batch">
+        <h2>
+          <span>LIMITED</span>
+          <span>BATCH</span>
+        </h2>
+      </div>      
+      <div class="fresh"><h3><a href="#fresh-panel">FRESH</a></h3></div>
+      <div class="organic"><h3><a href="#organic-panel">ORGANIC</a></h3></div>
+      <div class="transparent"><h3><a href="#transparent-panel">TRANSPARENT</a></h3></div>
+      <div class="fairtrade"><h3><a href="#fairtrade-panel">FAIRTRADE</a></h3></div>
+    </section>
     
-    <div class="limited-batch">
-      <h2>
-        <span>LIMITED</span>
-        <span>BATCH</span>
-      </h2>
-    </div>
-    
-    <div class="fresh"><h3><a href="">- FRESH</a></h3></div>
-    <div class="organic"><h3><a href="">- ORGANIC</a></h3></div>
-    <div class="transparent"><h3><a href="">- TRANSPARENT</a></h3></div>
-    <div class="organic"><h3><a href="">- FAIRTRADE</a></h3></div>
-    
+    <section id="fresh-panel" class="fresh-panel">
+      <h3>FRESH</h3>
+
+    </section>
+
+    <section id="organic-panel" class="organic-panel">
+      <h3>ORGANIC</h3>
+
+
+    </section>
+
+    <section id="transparent-panel"  class="transparent-panel">
+      <h3>TRANSPARENT</h3>
+
+
+    </section>
+
+    <section id="fairtrade-panel" class="fairtrade-panel">
+      <h3>FAIRTRADE</h3>
+
+
+    </section>
 
 
   </div>

a7c93f5b0e478c1ed577d42b2ba0a085d8adfc0d
venkatesh <venkatesh.caretiger@gmail.com>
2018-06-04 16:27:24 -0500

    signup form is setup

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,14 +4,12 @@
     <main>
         <section class=signup-wrapper>
 
-          <div class="signup-form">
-            <form action="/action_page.php">
-              First name: <input type="text" name="firstname" value="Mickey">
-              Last name: <input type="text" name="lastname" value="Mouse">              
-              <button type="submit" class="issecondary">Sign Up</button>
+            <form action="/catalog" class="form-wrapper">
+              <label class="email" for="email">Email:</label>
+              <input type="text" name="firstname" placeholder="mickey@example.com" class="email block" required>
+              <div class="g-recaptcha" data-sitekey="6LfpOF0UAAAAAK6KONqklR12-yktIQg8coz3Rp9b"></div>
+              <button type="submit" class="button isprimary half-block signup">Sign Up</button>
             </form> 
-          </div>
-
 
         </section>
     </main>

fd7f6df60cdab996dac1b47d8813c23b4c286f20
venkatesh <venkatesh.caretiger@gmail.com>
2018-06-06 22:47:32 -0500

    adding stuff and

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,91 +1,20 @@
 <template>
   <div class="home">
-
-    <section class="main">
-      <div class="spices">
-        <div class="site-title">
-          <h1 class="title">CANNON</h1>
-        </div>
-        <h1>
-          <span>SPICE</span>
-          <span>Co.</span>
-        </h1>
-      </div>      
-      <div class="benefits-title">
-        <h2>
-          <span>GOURMET</span>
-          <span>SPICES</span>
-        </h2>
-      </div>
-      <div class="benefits-wrapper">
-        <ul>
-          <li class="transparent"><h3><a href="#transparent-panel">$25 PER MONTH</a></h3></li>
-          <li class="fresh"><h3><a href="#fresh-panel">FINEST INGREDIENTS</a></h3></li>
-          <li class="organic"><h3><a href="#organic-panel">ORGANIC CERTIFIED</a></h3></li>
-          <li class="fairtrade"><h3><a href="#fairtrade-panel">FAIRTRADE COMPLIANT</a></h3></li>        
-          <li class="delivery"><h3><a href="#delivery-panel">FREE DELIVERY</a></h3></li>        
-        </ul>      
-      </div>
-    </section>
-    
-    <section id="fresh-panel" class="fresh-panel">
-      <div class="fresh-title">
-        <h2>FINEST INGREDIENTS</h2>
-      </div>
-      <div class="fresh-content">
-        <p>
-          Our Experts and Supplier Partners help us source the finest ingredients 
-          from all over the world.
-           Our Spices and Seasonings are top notch. 
-           The flavors they create will elevate your cooking to a whole new level.
-           Our spices and seasonings will go *BOOM* in your mouth.
-          <b><a href=#>SIGN UP</a></b></p>
-      </div>      
-      <div class="border-bottom"></div>
-    </section>
-
-    <section id="organic-panel" class="organic-panel">
-      <div class="organic-title">
-        <h2>ORGANIC</h2>
-      </div>
-      <div class="organic-content">
-        <p>No pesticides. bout how a food was grown free os pesticides.
-        
-        
-        </p>
-      </div>
-
-    </section>
-
-    <section id="pricing-panel"  class="transparent-panel">
-      <h3>TRANSPARENT</h3>
-    </section>
-
-    <section id="fairtrade-panel" class="fairtrade-panel">
-      <h3>FAIRTRADE</h3>
-      {{products}}
-    </section>
-
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
-import { db } from "../firebase.js";
-
 export default {
+  name: "Home",
   data() {
     return {
-      products: {},
-      msg: "hello venky"
+      msg: "Home"
     };
-  },
-
-  firebase: {
-    products: db.ref("Products")
   }
 };
 </script>
 
-<style lang="scss" scoped>
-@import "../styles/home.scss";
+<style lang="scss">
+
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="home">
+
+    <section class="main">
+      <div class="spices">
+        <div class="site-title-horizontal">
+          <h1 class="title">CANNON</h1>
+        </div>
+        <div class="site-title-vertical">
+          <h1>
+            <span>SPICE</span>
+            <span>Co.</span>
+          </h1>
+        </div>
+        <div class="benefits-title">
+          <h2>
+            <span>GOURMET</span>
+            <span>SPICES</span>
+          </h2>
+        </div>
+        <div class="benefits-wrapper">
+          <ul>
+            <li class="transparent"><h3><a href="#transparent-panel">$25 PER MONTH</a></h3></li>
+            <li class="fresh"><h3><a href="#fresh-panel">FINEST INGREDIENTS</a></h3></li>
+            <li class="organic"><h3><a href="#organic-panel">ORGANIC CERTIFIED</a></h3></li>
+            <li class="fairtrade"><h3><a href="#fairtrade-panel">FAIRTRADE COMPLIANT</a></h3></li>        
+            <li class="delivery"><h3><a href="#delivery-panel">FREE DELIVERY</a></h3></li>        
+          </ul>      
+        </div>
+      </div>      
+    </section>
+    
+    <section id="fresh-panel" class="fresh-panel">
+      <div class="fresh-title">
+        <h2>FINEST INGREDIENTS</h2>
+      </div>
+      <div class="fresh-content">
+        <p>
+          Our Experts and Supplier Partners help us source the finest ingredients 
+          from all over the world.
+           Our Spices and Seasonings are top notch. 
+           The flavors they create will elevate your cooking to a whole new level.
+           Our spices and seasonings will go *BOOM* in your mouth.
+          <b><a href=#>SIGN UP</a></b></p>
+      </div>      
+      <div class="border-bottom"></div>
+    </section>
+
+    <section id="organic-panel" class="organic-panel">
+      <div class="organic-title">
+        <h2>ORGANIC</h2>
+      </div>
+      <div class="organic-content">
+        <p>No pesticides. bout how a food was grown free os pesticides.
+        
+        
+        </p>
+      </div>
+
+    </section>
+
+    <section id="pricing-panel"  class="transparent-panel">
+      <h3>TRANSPARENT</h3>
+    </section>
+
+    <section id="fairtrade-panel" class="fairtrade-panel">
+      <h3>FAIRTRADE</h3>
+      {{products}}
+    </section>
+
+  </div>
+</template>
+
+<script>
+import { db } from "../firebase.js";
+
+export default {
+  data() {
+    return {
+      products: {},
+      msg: "hello venky"
+    };
+  },
+
+  firebase: {
+    products: db.ref("Products")
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/home.scss";
+</style>
